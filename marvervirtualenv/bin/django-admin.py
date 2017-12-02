4c0606de85d052352390f3dd1ad4b9310f93faee
Luca Licata <lucalicata@hotmail.com>
2017-12-02 11:41:41 +0000

    responsive design reviewed and improved

--- marvervirtualenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/diomede/Desktop/Projects/marverMultimedia/marvervirtualenv/bin/python3.5
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
