53728babe06efd7eae8c8aa6bccc2ed2a2496357
Luca Licata <lucalicata@hotmail.com>
2017-12-13 19:18:25 +0000

    changes to url file

---
+++ settings/config.py
@@ -0,0 +1,12 @@
+import os
+
+DATABASE_USERNAME = 'marver'
+DATABASE_PASSWORD = '2beornot2be'
+DATABASE_HOST = 'lucataproejctnew.c66082eq1miy.eu-west-2.rds.amazonaws.com'
+DATABASE_PORT = '3306'
+STRIPE_PUBLISHABLE = 'pk_test_hLMNTIjTI5BX4SLzE7buyXz4'
+STRIPE_SECRET =  'sk_test_4BdY6pB0aw6qgpvGYpriWfbu'
+AWS_ACCESS_KEY_ID = 'AKIAIBJCJA2RWV4CLKEQ'
+AWS_SECRET_ACCESS_KEY = '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'
+EMAIL_HOST_USER = 'lucadevelopment81@gmail.com'
+EMAIL_HOST_PASSWORD = '2beornot2be'
